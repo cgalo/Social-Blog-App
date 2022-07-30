07189673a1c88155deccd5d350cb8636137b2203
Carlos Galo <cgalo13@gmail.com>
2022-07-30 15:11:09 -0700

    Added toast library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import toast from 'react-hot-toast';
 
 import Loader from '../components/Loader';
+
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Loader isLoading={false} />
+      <button
+        onClick={() => toast.success('hello toast!')}
+      >
+        Toast Me
+      </button>
     </div>
   )
 }
